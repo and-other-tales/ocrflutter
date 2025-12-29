7b1ffab4e94fdb07c356e0214a678b9d0f8037c2
othertalesinc <david@othertales.co>
2025-12-29 23:17:52 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,6 +20,20 @@ const nextConfig = {
   // Configure for Cloud Run
   // Cloud Run provides PORT env var (8080 by default)
   // Next.js will use this automatically in standalone mode
+
+  // Build optimizations
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
+  },
+
+  // Reduce memory usage during build
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
+
+  // Disable SWC minification if it causes issues (use terser instead)
+  // swcMinify: false,
 }
 
 module.exports = nextConfig
